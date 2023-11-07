d0664835e400e5a175dccb05b8fc80e030cc59b9
Violin Yanev <violinyanev@users.noreply.github.com>
2023-11-07 22:23:48 +0100

    Add some data classes for a recipe

---
+++ app/src/main/java/com/example/myapplication/feature_recipe/domain/model/Recipe.kt
@@ -0,0 +1,18 @@
+package com.example.myapplication.feature_recipe.domain.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.myapplication.ui.theme.Purple80
+
+@Entity
+class Recipe(
+    val title: String,
+    val content: String,
+    val timestamp: Long,
+    val color: Int,
+    @PrimaryKey val id: Int? = null
+) {
+    companion object {
+        val recipeColors = listOf(Purple80)
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/feature_recipe/domain/model/RecipeDao.kt
@@ -0,0 +1,23 @@
+package com.example.myapplication.feature_recipe.domain.model
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface RecipeDao {
+    @Query("SELECT * FROM recipe")
+    fun getRecipes(): Flow<List<Recipe>>
+
+    @Query("SELECT * FROM recipe WHERE id =:id")
+    suspend fun getRecipeById(id: Int): Recipe?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertRecipe(recipe: Recipe)
+
+    @Delete
+    suspend fun deketeRecipe(recipe: Recipe)
+}
