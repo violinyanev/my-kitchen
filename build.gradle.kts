4e4607ffe42d820ec0cca4ac20f543b80a809159
Violin Yanev <violinyanev@users.noreply.github.com>
2023-12-01 09:27:19 +0100

    Add detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,4 +7,5 @@ plugins {
     alias(libs.plugins.ksp) apply false
     // alias(libs.plugins.android.test) apply false
     alias(libs.plugins.ktlint) apply false
+    alias(libs.plugins.detekt) apply false
 }
