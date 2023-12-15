75c7aba4f122ed00aa14b846d0a227816b66c0cf
Violin Yanev <violinyanev@users.noreply.github.com>
2023-12-15 18:28:22 +0100

    More wip

--- app/src/androidTest/java/com/example/myapplication/androidTest/java/com/example/myapplication/recipes/presentation/RecipesEndToEndTest.kt
+++ app/src/androidTest/java/com/example/myapplication/androidTest/java/com/example/myapplication/recipes/presentation/RecipesEndToEndTest.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication.recipes.presentation
 
+import android.content.res.Configuration
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertTextContains
@@ -9,15 +10,20 @@ import androidx.compose.ui.test.onAllNodesWithTag
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.onRoot
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTextInput
+import androidx.compose.ui.test.printToLog
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import androidx.test.platform.app.InstrumentationRegistry
 import com.example.myapplication.di.AppModule
 import com.example.myapplication.recipes.core.util.TestTags
+import com.example.myapplication.recipes.core.util.TestTags.CONTENT_TEXT_FIELD
+import com.example.myapplication.recipes.core.util.TestTags.TITLE_TEXT_FIELD
 import com.example.myapplication.recipes.presentation.editrecipe.AddEditRecipeScreen
 import com.example.myapplication.recipes.presentation.recipes.RecipeScreen
 import com.example.myapplication.recipes.presentation.util.Screen
@@ -28,133 +34,39 @@ import dagger.hilt.android.testing.UninstallModules
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.RuleChain
+
 
 @HiltAndroidTest
-class RecipesEndToEndTest {
+class MainActivityTest {
 
-    @get:Rule(order = 0)
-    val hiltRule = HiltAndroidRule(this)
+    private val hiltRule = HiltAndroidRule(this)
+    private val composeTestRule = createAndroidComposeRule<MainActivity>()
 
-    @get:Rule(order = 1)
-    val composeRule = createAndroidComposeRule<MainActivity>()
+    @get:Rule
+    val rule: RuleChain = RuleChain
+        .outerRule(hiltRule)
+        .around(composeTestRule)
 
-    @ExperimentalAnimationApi
-    @Before
-    fun setUp() {
-        hiltRule.inject()
-        composeRule.setContent {
-            MyApplicationTheme {
-                val navController = rememberNavController()
-                NavHost(
-                    navController = navController,
-                    startDestination = Screen.RecipesScreen.route
-                ) {
-                    composable(route = Screen.RecipesScreen.route) {
-                        RecipeScreen(navController = navController)
-                    }
-                    composable(
-                        route = Screen.AddEditRecipeScreen.route +
-                            "?recipeId={recipeId}&recipeColor={recipeColor}",
-                        arguments = listOf(
-                            navArgument(
-                                name = "recipeId"
-                            ) {
-                                type = NavType.IntType
-                                defaultValue = -1
-                            },
-                            navArgument(
-                                name = "recipeColor"
-                            ) {
-                                type = NavType.IntType
-                                defaultValue = -1
-                            }
-                        )
-                    ) {
-                        val color = it.arguments?.getInt("recipeColor") ?: -1
-                        AddEditRecipeScreen(
-                            navController = navController,
-                            recipeColor = color
-                        )
-                    }
-                }
-            }
-        }
-    }
+    @Test fun clickAddRecipe_OpensNewRecipeScreen() {
+        composeTestRule.onRoot().printToLog("TEST")
 
-    @Test
-    fun saveNewRecipe_editAfterwards() {
-        // Click on FAB to get to add recipe screen
-        composeRule.onNodeWithContentDescription("Add").performClick()
-
-        // Enter texts in title and content text fields
-        /*composeRule
-            .onNodeWithTag(TestTags.TITLE_TEXT_FIELD)
-            .performTextInput("test-title")
-        composeRule
-            .onNodeWithTag(TestTags.CONTENT_TEXT_FIELD)
-            .performTextInput("test-content")
-        // Save the new
-        composeRule.onNodeWithContentDescription("Save").performClick()
-
-        // Make sure there is a recipe in the list with our title and content
-        composeRule.onNodeWithText("test-title").assertIsDisplayed()
-        // Click on recipe to edit it
-        composeRule.onNodeWithText("test-title").performClick()
-
-        // Make sure title and content text fields contain recipe title and content
-        composeRule
-            .onNodeWithTag(TestTags.TITLE_TEXT_FIELD)
-            .assertTextEquals("test-title")
-        composeRule
-            .onNodeWithTag(TestTags.CONTENT_TEXT_FIELD)
-            .assertTextEquals("test-content")
-        // Add the text "2" to the title text field
-        composeRule
-            .onNodeWithTag(TestTags.TITLE_TEXT_FIELD)
-            .performTextInput("2")
-        // Update the recipe
-        composeRule.onNodeWithContentDescription("Save").performClick()
-
-        // Make sure the update was applied to the list
-        composeRule.onNodeWithText("test-title2").assertIsDisplayed()*/
-    }
-
-    @Test
-    fun saveNewRecipes_orderByTitleDescending() {
-        for (i in 1..3) {
-            // Click on FAB to get to add recipe screen
-            composeRule.onNodeWithContentDescription("Add").performClick()
-
-            // Enter texts in title and content text fields
-            composeRule
-                .onNodeWithTag(TestTags.TITLE_TEXT_FIELD)
-                .performTextInput(i.toString())
-            composeRule
-                .onNodeWithTag(TestTags.CONTENT_TEXT_FIELD)
-                .performTextInput(i.toString())
-            // Save the new
-            composeRule.onNodeWithContentDescription("Save").performClick()
+        // When the "Add" button is clicked
+        with(composeTestRule.onNodeWithText("Add recipe")) {
+            assertExists()
+            assertIsDisplayed()
+            performClick()
         }
 
-        composeRule.onNodeWithText("1").assertIsDisplayed()
-        composeRule.onNodeWithText("2").assertIsDisplayed()
-        composeRule.onNodeWithText("3").assertIsDisplayed()
+        composeTestRule.waitForIdle()
 
-        composeRule
-            .onNodeWithContentDescription("Sort")
-            .performClick()
-        composeRule
-            .onNodeWithContentDescription("Title")
-            .performClick()
-        composeRule
-            .onNodeWithContentDescription("Descending")
-            .performClick()
-
-        composeRule.onAllNodesWithTag(TestTags.RECIPE_ITEM)[0]
-            .assertTextContains("3")
-        composeRule.onAllNodesWithTag(TestTags.RECIPE_ITEM)[1]
-            .assertTextContains("2")
-        composeRule.onAllNodesWithTag(TestTags.RECIPE_ITEM)[2]
-            .assertTextContains("1")
+        with(composeTestRule.onNodeWithText(TITLE_TEXT_FIELD)) {
+            assertExists()
+            assertIsDisplayed()
+        }
+        with(composeTestRule.onNodeWithText(CONTENT_TEXT_FIELD)) {
+            assertExists()
+            assertIsDisplayed()
+        }
     }
 }
