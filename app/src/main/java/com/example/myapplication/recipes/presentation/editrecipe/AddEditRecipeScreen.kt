31145eebc5bfa7f0ab594f00e1b9639aeedd8cc2
Violin Yanev <violinyanev@users.noreply.github.com>
2024-01-07 18:39:45 +0100

    Fix formatting

--- app/src/main/java/com/example/myapplication/recipes/presentation/editrecipe/AddEditRecipeScreen.kt
+++ app/src/main/java/com/example/myapplication/recipes/presentation/editrecipe/AddEditRecipeScreen.kt
@@ -24,7 +24,6 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.onFocusChanged
-import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
