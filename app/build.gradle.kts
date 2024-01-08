d6f171acc73d19cb9bf4045597d7cf9422a96cfa
Violin Yanev <3830505+violinyanev@users.noreply.github.com>
2024-01-08 15:40:32 +0100

    Fix filter icon (#42)
    Co-authored-by: Violin Yanev <violinyanev@users.noreply.github.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,7 @@ dependencies {
     implementation(libs.androidx.paging.compose)
     implementation(libs.androidx.room.ktx)
     implementation(libs.androidx.work.runtime.ktx)
+    implementation(libs.androidx.compose.material.icons.ext)
     implementation(libs.material)
     // implementation(libs.gson)
     // implementation(libs.okhttp3.logging.interceptor)
--- app/src/main/java/com/example/myapplication/recipes/presentation/recipes/components/OrderSection.kt
+++ app/src/main/java/com/example/myapplication/recipes/presentation/recipes/components/OrderSection.kt
@@ -9,10 +9,12 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.myapplication.R
 import com.example.myapplication.recipes.domain.util.OrderType
 import com.example.myapplication.recipes.domain.util.RecipeOrder
+import com.example.myapplication.ui.theme.MyApplicationTheme
 
 @Composable
 fun OrderSection(
@@ -61,5 +63,16 @@ fun OrderSection(
                 }
             )
         }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun OrderSectionPreview() {
+    MyApplicationTheme {
+        OrderSection(
+            recipeOrder = RecipeOrder.Date(OrderType.Descending),
+            onOrderChange = {}
+        )
     }
 }
