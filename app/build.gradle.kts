870af35e6dccea0bc1bc7c5d1b2158c619615c25
Violin Yanev <3830505+violinyanev@users.noreply.github.com>
2024-01-09 15:57:53 +0100

    Release build shrinks resources (#46)
    Co-authored-by: Violin Yanev <violinyanev@users.noreply.github.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,7 +30,19 @@ android {
     }
     buildTypes {
         release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            signingConfig = signingConfigs.getByName("debug")
+        }
+        debug {
             isMinifyEnabled = false
+            isShrinkResources = false
+
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
