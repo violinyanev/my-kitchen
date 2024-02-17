a856fc4bf04327ff644732f03f8892c4b0afed12
Violin Yanev <3830505+violinyanev@users.noreply.github.com>
2024-02-17 16:30:19 +0100

    Add login support and backend tests (#57)
    Co-authored-by: Violin Yanev <violinyanev@users.noreply.github.com>

--- app/src/main/java/com/example/myapplication/recipes/data/datasource/backend/RecipeService.kt
+++ app/src/main/java/com/example/myapplication/recipes/data/datasource/backend/RecipeService.kt
@@ -8,12 +8,16 @@ import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface RecipeService {
+
     @GET("/recipes")
     fun getRecipes(): Call<List<BackendRecipe>>
 
     @POST("/recipes")
-    fun createRecipe(@Body recipeRequest: BackendRecipe): Call<BackendRecipe>
+    fun createRecipe(@Body recipeRequest: BackendRecipe): Call<BackendRecipeResponse>
 
     @DELETE("/recipes/{recipeId}")
-    fun deleteRecipe(@Path("recipeId") recipeId: Int): Call<Void>
+    fun deleteRecipe(@Path("recipeId") recipeId: Long): Call<BackendRecipeResponse>
+
+    @POST("/users/login")
+    fun login(@Body loginRequest: LoginRequest): Call<LoginResult>
 }
