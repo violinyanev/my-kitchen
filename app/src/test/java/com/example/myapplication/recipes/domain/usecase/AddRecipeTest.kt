54e3af3147436c02402ccc81d9dd02c1820cf532
Violin Yanev <3830505+violinyanev@users.noreply.github.com>
2024-02-20 21:05:06 +0100

    Add some unit tests (#71)
    * Add some unit tests
    
    * Fix formatting
    
    ---------
    
    Co-authored-by: Violin Yanev <violinyanev@users.noreply.github.com>

---
+++ app/src/test/java/com/example/myapplication/recipes/domain/usecase/AddRecipeTest.kt
@@ -0,0 +1,27 @@
+package com.example.myapplication.recipes.domain.usecase
+
+import com.example.myapplication.recipes.data.repository.FakeRecipeRepository
+import com.example.myapplication.recipes.domain.model.Recipe
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+
+class AddRecipeTest {
+
+    private lateinit var addRecipe: AddRecipe
+    private lateinit var fakeRepository: FakeRecipeRepository
+
+    @Before
+    fun setUp() {
+        fakeRepository = FakeRecipeRepository()
+        addRecipe = AddRecipe(fakeRepository)
+    }
+
+    @Test
+    fun `Add recipe`() = runBlocking {
+        val recipe = Recipe(title = "test", content = "content", id = 5L, timestamp = 15L)
+        addRecipe(recipe)
+        assertThat(recipe).isIn(fakeRepository.getTestRecipes())
+    }
+}
