0e086196e3d5fb7408e589cd29b241627c3b9c57
Violin Yanev <violinyanev@users.noreply.github.com>
2024-03-01 20:20:35 +0100

    WIP

--- app/src/main/kotlin/com/ultraviolince/mykitchen/recipes/presentation/recipes/components/Dismissable.kt
+++ app/src/main/kotlin/com/ultraviolince/mykitchen/recipes/presentation/recipes/components/Dismissable.kt
@@ -5,11 +5,15 @@ import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -32,8 +36,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import com.ultraviolince.mykitchen.recipes.domain.model.Recipe
+import com.ultraviolince.mykitchen.recipes.domain.util.RecipeOrder
+import com.ultraviolince.mykitchen.recipes.presentation.recipes.RecipesEvent
+import com.ultraviolince.mykitchen.ui.theme.MyApplicationTheme
 import kotlinx.coroutines.delay
 
 
@@ -107,5 +115,37 @@ fun DeleteBackground(
             contentDescription = null,
             tint = Color.White
         )
+    }
+}
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun DismissablePreview(
+) {
+    MyApplicationTheme {
+        val recipes =  MutableList(3) { index ->
+                Recipe(
+                    "Recipe $index",
+                    content = "Lorem ipsum dolor sit amet $index",
+                    timestamp = 5
+                )
+        }
+        LazyColumn {
+            items(recipes) { recipe ->
+                Dismissable(item = recipe,
+                    onDelete = {
+                        recipes.remove(it)
+                    }
+                ) {
+                    RecipeItem(
+                        recipe = recipe,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable {
+                            })
+                }
+            }
+        }
     }
 }
