1038e03beb2d1b872376a6c7ae8ddf68b4757ecb
Violin Yanev <violinyanev@users.noreply.github.com>
2024-07-09 22:11:29 +0200

    Fix ktlint issues

--- app/src/androidTest/java/com/ultraviolince/mykitchen/recipes/utils/TestRunner.kt
+++ app/src/androidTest/java/com/ultraviolince/mykitchen/recipes/utils/TestRunner.kt
@@ -1,8 +1,5 @@
 package com.ultraviolince.mykitchen.recipes.utils
 
-import android.app.Application
-import android.content.Context
 import androidx.test.runner.AndroidJUnitRunner
 
-class TestRunner : AndroidJUnitRunner() {
-}
+class TestRunner : AndroidJUnitRunner()
