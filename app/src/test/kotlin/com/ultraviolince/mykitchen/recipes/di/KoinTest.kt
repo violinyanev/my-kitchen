9dda247f8caee61885d2fde834e26c90bf438ca7
Violin Yanev <violinyanev@users.noreply.github.com>
2024-07-15 22:20:06 +0200

    Fix test

--- app/src/test/kotlin/com/ultraviolince/mykitchen/recipes/di/KoinTest.kt
+++ app/src/test/kotlin/com/ultraviolince/mykitchen/recipes/di/KoinTest.kt
@@ -4,6 +4,7 @@ import com.ultraviolince.mykitchen.di.AppModule
 import org.junit.Test
 import org.koin.core.annotation.KoinExperimentalAPI
 import org.koin.ksp.generated.module
+import org.koin.ksp.generated.com_ultraviolince_mykitchen_di_AppModule
 import org.koin.test.KoinTest
 import org.koin.test.verify.verify
 
@@ -12,6 +13,6 @@ class KoinTest : KoinTest {
     @OptIn(KoinExperimentalAPI::class)
     @Test
     fun checkAllModules() {
-        AppModule().module.verify()
+        AppModule().module.verify(extraTypes = listOf(androidx.lifecycle.SavedStateHandle::class))
     }
 }
