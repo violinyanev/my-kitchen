7c19ab7711860dd6fd3205124cb8ec281db5fc71
Violin Yanev <violinyanev@users.noreply.github.com>
2024-07-15 21:53:17 +0200

    missing include

--- app/src/test/kotlin/com/ultraviolince/mykitchen/recipes/di/KoinTest.kt
+++ app/src/test/kotlin/com/ultraviolince/mykitchen/recipes/di/KoinTest.kt
@@ -3,6 +3,7 @@ package com.ultraviolince.mykitchen.recipes.di
 import com.ultraviolince.mykitchen.di.AppModule
 import org.junit.Test
 import org.koin.core.annotation.KoinExperimentalAPI
+import org.koin.ksp.generated.module
 import org.koin.test.KoinTest
 import org.koin.test.verify.verify
 
