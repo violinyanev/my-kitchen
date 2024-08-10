3f01cda43a3659a80f1f0d5b9467703cf6d3734f
Violin Yanev <violinyanev@users.noreply.github.com>
2024-08-10 23:11:40 +0200

    Migrate plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,15 @@ plugins {
     // in each subproject's classloader
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.androidLibrary) apply false
-    alias(libs.plugins.jetbrainsCompose) apply false
+    alias(libs.plugins.build.health) apply true
     alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.detekt) apply false
+    alias(libs.plugins.jetbrainsCompose) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.ktlint) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
+    alias(libs.plugins.room) apply false
+    // alias(libs.plugins.android.test) apply false
 }
