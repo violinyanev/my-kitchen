d504b6c499960ff6aa61165732f0e54b4c650e44
Violin Yanev <violinyanev@users.noreply.github.com>
2024-08-16 18:35:01 +0200

    Remove desktop for now

--- composeApp/src/desktopMain/kotlin/KoinInitializer.desktop.kt
+++
@@ -1,7 +0,0 @@
-@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
-actual class KoinInitializer {
-    actual fun init() {
-        // TODO VIEWMODELS
-        // modules(appModule, viewModelModule)
-    }
-}
