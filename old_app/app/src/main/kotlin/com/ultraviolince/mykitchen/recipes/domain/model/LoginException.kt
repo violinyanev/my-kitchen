050be6c5623d2d12be8dbef08d4da6b30220e32f
Violin Yanev <3830505+violinyanev@users.noreply.github.com>
2024-08-18 19:07:59 +0200

    Cleanup old app (#161)
    * Add (fake) Dao and database
    
    * Add recipes screen
    
    * Fix icon
    
    * Delete migrated classes
    
    ---------
    
    Co-authored-by: Violin Yanev <violinyanev@users.noreply.github.com>

--- old_app/app/src/main/kotlin/com/ultraviolince/mykitchen/recipes/domain/model/LoginException.kt
+++
@@ -1,5 +0,0 @@
-package com.ultraviolince.mykitchen.recipes.domain.model
-
-import androidx.annotation.StringRes
-
-class LoginException(@StringRes val errorMsg: Int) : Exception()
--- old_app/app/src/main/kotlin/com/ultraviolince/mykitchen/recipes/presentation/util/Screen.kt
+++
@@ -1,7 +0,0 @@
-package com.ultraviolince.mykitchen.recipes.presentation.util
-
-sealed class Screen(val route: String) {
-    object LoginScreen : Screen("login_screen")
-    object RecipesScreen : Screen("recipes_screen")
-    object AddEditRecipeScreen : Screen("add_edit_recipe_screen")
-}
