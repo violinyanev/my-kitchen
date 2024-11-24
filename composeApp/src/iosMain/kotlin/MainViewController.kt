3f0857213ea0e2dc445e94068ab32d630cbe8c1e
Violin Yanev <violinyanev@users.noreply.github.com>
2024-11-24 18:46:00 +0100

    Lowercase function iOS

--- composeApp/src/iosMain/kotlin/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/MainViewController.kt
@@ -1,6 +1,6 @@
 import androidx.compose.ui.window.ComposeUIViewController
 
-fun MainViewController() =
+fun mainViewController() =
     ComposeUIViewController(
         configure = {
             KoinInitializer().init()
