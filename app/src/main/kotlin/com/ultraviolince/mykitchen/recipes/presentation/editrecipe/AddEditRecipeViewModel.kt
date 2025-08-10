3d6a2c089d522ca3046f05b9b7a939378b6f311f
Violin Yanev <violinyanev@users.noreply.github.com>
2025-08-10 15:06:31 +0200

    fix: Fix dependency injection declarations

--- app/src/main/kotlin/com/ultraviolince/mykitchen/recipes/presentation/editrecipe/AddEditRecipeViewModel.kt
+++ app/src/main/kotlin/com/ultraviolince/mykitchen/recipes/presentation/editrecipe/AddEditRecipeViewModel.kt
@@ -15,11 +15,12 @@ import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
 import org.koin.android.annotation.KoinViewModel
+import org.koin.core.annotation.InjectedParam
 
 @KoinViewModel
 class AddEditRecipeViewModel(
     private val recipesUseCases: Recipes,
-    savedStateHandle: SavedStateHandle
+    @InjectedParam savedStateHandle: SavedStateHandle
 ) : ViewModel() {
     private val _recipeTitle = mutableStateOf(
         RecipeTextFieldState(
