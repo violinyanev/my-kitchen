7390f6c96362090c0bf5bf142d8c80dc056bc128
Violin Yanev <violinyanev@users.noreply.github.com>
2026-06-04 21:15:47 +0200

    build: configure root build for multi-module KMP project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,42 +1,26 @@
 pluginManagement {
+    includeBuild("build-logic")
     repositories {
         google()
         mavenCentral()
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
         google()
         mavenCentral()
-        // Required for Kotlin/JS Node.js distribution
-        exclusiveContent {
-            forRepository {
-                ivy("https://nodejs.org/dist/") {
-                    name = "Node Distributions at https://nodejs.org/dist"
-                    patternLayout { artifact("v[revision]/[artifact](-v[revision]-[classifier]).[ext]") }
-                    metadataSources { artifact() }
-                    content { includeModule("org.nodejs", "node") }
-                }
-            }
-            filter { includeGroup("org.nodejs") }
-        }
-        // Required for Kotlin/JS Yarn distribution
-        exclusiveContent {
-            forRepository {
-                ivy("https://github.com/yarnpkg/yarn/releases/download/") {
-                    name = "Yarn Distributions at https://github.com/yarnpkg/yarn/releases/download"
-                    patternLayout { artifact("v[revision]/[artifact](-v[revision]).[ext]") }
-                    metadataSources { artifact() }
-                    content { includeModule("com.yarnpkg", "yarn") }
-                }
-            }
-            filter { includeGroup("com.yarnpkg") }
-        }
     }
 }
 
 rootProject.name = "my-kitchen"
-include(":app")
-include(":shared")
+
+include(":shared:domain")
+include(":shared:data")
+include(":shared:ui")
+include(":androidApp")
+include(":desktopApp")
+include(":webApp")
+include(":server")
